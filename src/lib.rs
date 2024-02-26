d22e59b069bde0b512dcf290e875af1470ac6290
lake <102535481@qq.com>
2024-02-26 17:06:31 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,18 @@ ios pngdefry tool for rust
 [![QQ群](https://img.shields.io/badge/QQ%E7%BE%A4-799168925-blue)](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=dLoye8pBcO60zGzqLjGO0l-GgMIaf6wQ&authKey=LfxBdZ5A%2F9eWJbKpzTcuWPjmQu5UdIJ3TVTpqRAQYkCID50WLkYoIXcGxGKzupG3&noverify=0&group_code=799168925)
 
 # 使用指南
+判断是不是iphone png
+```rust
+let result = pngdefry::iphone_png("./iphone.png").unwrap();
+if(result){
+    println!("iphone png");
+}else{
+    println!("not iphone png");
+}
 ```
-let result = pngdefry::convert("./iphone.png").unwrap();
-let mut is_png = pngdefry::iphone_png("./iphone.png").unwrap();
-println!("iphone is_png: {}", is_png);
-is_png = pngdefry::iphone_png("./img.png").unwrap();
-println!("niphone is_png: {}", is_png);
+还原iphone png
+```
+pngdefry::convert("./iphone.png","./big.png").unwrap();
 ```
 */
 pub mod pngdefry;
