9b8e65b954ca9158825e400e86ce4b55066c7a59
lake <102535481@qq.com>
2024-04-04 12:47:23 +0800

    init

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,21 @@
+use std::fmt::{Display, Formatter};
+
 #[derive(Debug)]
 pub enum PngdefryError {
     Msg(String),
 }
 
+impl Display for PngdefryError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PngdefryError::Msg(msg) => write!(f, "{}", msg),
+        }
+    }
+}
+
+impl std::error::Error for PngdefryError {}
+
 unsafe impl Send for PngdefryError {}
+
 unsafe impl Sync for PngdefryError {}
 
