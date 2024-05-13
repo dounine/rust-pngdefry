093102835231ab4a040718d2c09b9dfe7fa79191
lake <102535481@qq.com>
2024-05-13 15:15:00 +0800

    优化

--- src/pngdefry.rs
+++ src/pngdefry.rs
@@ -3,12 +3,12 @@ use crate::error::PngdefryError;
 
 include!("../bindings/bindings.rs");
 
-pub fn convert<T>(file_path: T, output_path: T) -> Result<(), PngdefryError>
-    where T: AsRef<str>
-{
+pub fn convert<T: AsRef<Path> + ?Sized>(file_path: &T, output_path: &T) -> Result<(), PngdefryError> {
     let mut error_mut: [std::os::raw::c_char; 1024] = [0; 1024];
-    let file_path = std::ffi::CString::new(file_path.as_ref()).map_err(|e| PngdefryError::Msg(e.to_string()))?;
-    let output_path = std::ffi::CString::new(output_path.as_ref()).map_err(|e| PngdefryError::Msg(e.to_string()))?;
+    let file_path_str = file_path.as_ref().to_string_lossy();
+    let output_path_str = output_path.as_ref().to_string_lossy();
+    let file_path = std::ffi::CString::new(file_path_str.to_string()).map_err(|e| PngdefryError::Msg(e.to_string()))?;
+    let output_path = std::ffi::CString::new(output_path_str.to_string()).map_err(|e| PngdefryError::Msg(e.to_string()))?;
     unsafe {
         restore_png(file_path.as_ptr(), output_path.as_ptr(), error_mut.as_mut_ptr());
         let error_msg = std::ffi::CStr::from_ptr(error_mut.as_ptr())
@@ -22,8 +22,8 @@ pub fn convert<T>(file_path: T, output_path: T) -> Result<(), PngdefryError>
 }
 
 pub fn iphone_png<T: AsRef<Path> + ?Sized>(file_path: &T) -> Result<bool, PngdefryError> {
-    let path = file_path.as_ref().to_string_lossy();
-    let file_path = std::ffi::CString::new(path.to_string()).map_err(|e| PngdefryError::Msg(e.to_string()))?;
+    let file_path_str = file_path.as_ref().to_string_lossy();
+    let file_path = std::ffi::CString::new(file_path_str.to_string()).map_err(|e| PngdefryError::Msg(e.to_string()))?;
     unsafe {
         let mut error_mut: [std::os::raw::c_char; 1024] = [0; 1024];
         let result = is_iphone_png(file_path.as_ptr(), error_mut.as_mut_ptr());
